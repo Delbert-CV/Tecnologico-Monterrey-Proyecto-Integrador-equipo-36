d6005fbabff2938ccde55af862a19b94c1ac0675
Delbert-CV <A01795613@tec.mx>
2025-10-14 17:14:54 -0600

    1st model, also builders and downloaders of wdata

---
+++ Historicos de lluvia Gobierno de Mexico/smn_temp_builder.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import unicodedata
+import time
+import os
+import re
+
+#  IMPORTANTE:
+#       - Este Script debe usarse después de descargar la data por medio del script smn_precipitation_downloader.py 
+#       - Abril de 2023 tiene las columnas en desorden, por lo que corregimos las mismas manualmente, y es  necesario tomar esto en cuenta si se quiere correr este script.
+
+def remove_accents(text):
+    if pd.isna(text):
+        return text
+    
+    return ''.join(
+        c for c in unicodedata.normalize('NFKD', str(text))
+        if not unicodedata.combining(c)
+    )
+
+def clean_text(text):
+    if pd.isna(text):
+        return text
+    text = remove_accents(text)
+    return re.sub(r'[^A-Za-z0-9\s]', '', text).upper()
+
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+data_dir    = os.path.join(current_dir, "descargas_smn")
+available_temps = ['TMed', 'TMax', 'TMin']
+
+years  =  range(2014,2026)
+months = range(1,13)
+
+all_df = []
+
+for type_of_temp in available_temps:
+    all_df = []
+    
+    for year in years:
+        for month in months:
+            path_to_load = os.path.join(data_dir, f"{year}_{month:02d}_000_{type_of_temp}.csv")
+            
+            
+            if os.path.exists(path_to_load):
+                df = pd.read_csv(path_to_load, encoding='latin1', usecols=range(6), engine='python')
+                df.columns = ['lon', 'lat', 'state', 'cve_sih', 'name', f'{type_of_temp}'] 
+                df['year-month'] = f"{year}-{month:02d}" 
+                all_df.append(df)
+            
+    df_final  = pd.concat(all_df, ignore_index=True)
+
+    string_cols = ['state', 'cve_sih', 'name']
+
+    for col in string_cols:
+        df_final[col] = df_final[col].apply(remove_accents)
+    
+    for col in string_cols:
+        df_final[col] = df_final[col].apply(clean_text)
+
+    df_final.to_csv(f'{type_of_temp}_per_area_historical.csv', index=False)
+        
---
+++ Historicos de lluvia Gobierno de Mexico/smn_temp_downloader.py
@@ -0,0 +1,48 @@
+import os
+import requests
+import time
+
+# Importante:
+# - Este script está pensado para crear el folder "descargas_smn" en el mismo directorio donde se corra este script.
+# - Esta información se procesa luego con el script smn_temp_builder.py, el cual consolida toda la data de temperaturas
+#   maximas, minimas y media disponibles en la pagina del SMN (Servicio Meteorológica Nacional)
+
+# Folder sobre el que se descargará la información de temperatura por estado desde la pagina de la SMN
+os.makedirs("descargas_smn", exist_ok=True)
+
+# Rango de años y meses que se quieren descargar
+years = range(2014, 2026)
+months = range(1, 13)
+
+# Tipos de temperaturas a descargar
+available_temps = ['TMed', 'TMax', 'TMin']
+
+# Ciclo principal de descarga
+for type_of_temp in available_temps:
+    for year in years:
+        for month in months:
+            
+            #  URL para descargar los archivos CSV
+            
+            url = f"https://smn.conagua.gob.mx/tools/RESOURCES/com_archivo_datos_resumenes/{year}{month:02d}010000{type_of_temp}.csv"
+            
+            # Nombre del archivo a descargar
+            filename = f"descargas_smn/{year}_{month:02d}_000_{type_of_temp}.csv"
+            
+            try:
+                r = requests.get(url, timeout=10)
+                
+                if r.status_code == 200 and len(r.content) > 50:
+                    with open(filename, "wb") as f:
+                        f.write(r.content)
+                    
+                    print(f"✅ Se descargó {filename}")
+                
+                else:
+                    print(f"⚠️ No se encontró: {url}")
+                    
+                    
+            except Exception as e:
+                print(f"❌ Hubo un error con {url}: {e}")
+            
+            time.sleep(10)
